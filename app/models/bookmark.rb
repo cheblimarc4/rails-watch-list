9a561ebb4a3342f7cc63000a421b252fead614aa
Marc Chebli <cheblimarc4@gmail.com>
2024-08-16 12:46:30 +0100

    first try

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,4 +3,5 @@ class Bookmark < ApplicationRecord
   belongs_to :list
   validates :movie_id, uniqueness: { scope: :list_id, message: " list combination must be unique" }
   validates :comment, length: { minimum: 6 }
+  has_one_attached :photo
 end
